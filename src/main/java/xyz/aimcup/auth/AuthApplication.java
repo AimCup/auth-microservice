8ee6a6badb8abb19bca7e7d330f7d1d61633757f
Adam Orłowski <orlowski.adam.99@gmail.com>
2023-10-11 21:38:18 +0200

    [AC-7] Added @ComponentScan to look for Security Configuration Beans

--- src/main/java/xyz/aimcup/auth/AuthApplication.java
+++ src/main/java/xyz/aimcup/auth/AuthApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EntityScan(basePackages = {"xyz.aimcup.security.domain"})
+@ComponentScan(basePackages = {"xyz.aimcup.security", "xyz.aimcup.auth"})
 public class AuthApplication {
     public static void main(String[] args) {
         SpringApplication.run(AuthApplication.class, args);
